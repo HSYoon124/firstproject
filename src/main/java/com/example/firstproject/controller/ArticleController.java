230647e86e7b225e60bda63df3f7082006992697
HSYoon <yhs040421@ewha.ac.kr>
2026-06-20 22:23:09 +0900

    feat: 스프링 부트3 Part2 6~8 study

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -40,7 +41,7 @@ public class ArticleController {
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
         System.out.println(saved.toString());*/
-        return "";
+        return "redirect:/articles/"+saved.getId();
     }
 
     @GetMapping("/articles/{id}")
@@ -63,4 +64,44 @@ public class ArticleController {
         // 3. 뷰 페이지 설정하기
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        //수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 뷰 페이지 설정하기
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+        //1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        //2. 엔티티를 DB에 저장하기
+        Article target =  articleRepository.findById(articleEntity.getId()).orElse(null);
+        if (target != null){
+            articleRepository.save(articleEntity);
+        }
+        return "redirect:/articles/"+target.getId();
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다!!");
+        //1, 삭제할 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        //2. 대상 엔티티 삭제하기
+        if (target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다!");
+        }
+        //3. 결과 페이지로 리다이렉트하기
+        return "redirect:/articles";
+    }
+
 }
